5f5137bd111f08aba370b0544e26da3bb1542477
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2021-08-19 11:41:40 -0300

    Arrays

---
+++ Java-Detail/src/br/com/abc/introducao/arrays/Arrays1.java
@@ -0,0 +1,15 @@
+package br.com.abc.introducao.arrays;
+
+public class Arrays1 {
+    public static void main(String[] args) {
+        int[] idades = new int[3];
+        idades[0] = 20;
+        idades[1] = 15;
+        idades[2] = 30;
+        System.out.println("idade 1: "+idades[0]);
+        System.out.println("idade 2: "+idades[1]);
+        System.out.println("idade 3: "+idades[2]);
+
+
+    }
+}
