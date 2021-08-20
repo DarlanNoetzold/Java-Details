3c71717ad63bfb73350ef438051ea8dfb54a57bc
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2021-08-20 17:58:47 -0300

    Array

--- Java-Detail/src/br/com/abc/introducao/arrays/Arrays4.java
+++ Java-Detail/src/br/com/abc/introducao/arrays/Arrays4.java
@@ -1,8 +1,5 @@
 package br.com.abc.introducao.arrays;
 
-/**
- * Created by William Suane on 3/3/2016.
- */
 public class Arrays4 {
     public static void main(String[] args) {
         String[] nomes = {"Juju", "Jao Jao", "Xu xu"};
