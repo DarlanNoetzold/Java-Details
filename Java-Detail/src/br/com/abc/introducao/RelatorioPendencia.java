fcbc2648839b43336f2bf92a23cc7b62fab0bf63
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2021-08-21 09:54:48 -0300

    Operadores

---
+++ Java-Detail/src/br/com/abc/introducao/RelatorioPendencia.java
@@ -0,0 +1,13 @@
+package br.com.abc.introducao;
+
+
+public class RelatorioPendencia {
+    public static void main(String[] args) {
+        String nome = "Bulma";
+        String endereco = "439 East Distric";
+        String telefone = "(11)1234-5689";
+        System.out.println("O "+nome+" domiciliado no endereco "+ endereco +" e telefone "+telefone +" Nao possui pendencias");
+
+
+    }
+}
---
+++ Java-Detail/src/br/com/abc/introducao/operadores/OperadoresLogicos.java
@@ -0,0 +1,9 @@
+package br.com.abc.introducao.operadores;
+
+public class OperadoresLogicos {
+    public static void main(String[]  args){
+        int idade = 18;
+        float salario = 5000f;
+        System.out.println(idade >= 16 & salario >= 2000);
+    }
+}
