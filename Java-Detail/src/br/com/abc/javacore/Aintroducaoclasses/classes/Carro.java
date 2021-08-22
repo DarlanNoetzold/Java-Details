6d3bc2330f8a0c7439062479a325ef49e89be083
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2021-08-22 08:48:49 -0300

    Classes

---
+++ Java-Detail/src/br/com/abc/javacore/Aintroducaoclasses/classes/Carro.java
@@ -0,0 +1,8 @@
+package br.com.abc.javacore.Aintroducaoclasses.classes;
+
+public class Carro {
+    public String placa;
+    public String modelo;
+    public float velocidadeMaxima;
+
+}
---
+++ Java-Detail/src/br/com/abc/javacore/Aintroducaoclasses/classes/Professor.java
@@ -0,0 +1,14 @@
+package br.com.abc.javacore.Aintroducaoclasses.classes;
+
+// Crie os seguintes atributos para essa classe
+    //Nome
+    //Matricula
+    //rg
+    //cpf
+// Crie uma classe de teste para preencher e imprimir os dados desse professor
+public class Professor {
+    public String nome;
+    public String matricula;
+    public String rg;
+    public String cpf;
+}
