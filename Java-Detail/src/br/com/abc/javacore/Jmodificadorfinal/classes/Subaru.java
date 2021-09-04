ae6a692d1239ffdf629b630cc5d5b06aef3f74a3
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2021-09-04 07:35:46 -0300

    ModificadorFinal

---
+++ Java-Detail/src/br/com/abc/javacore/Jmodificadorfinal/classes/Subaru.java
@@ -0,0 +1,8 @@
+package br.com.abc.javacore.Jmodificadorfinal.classes;
+
+public class Subaru extends Carro{
+
+    public void imprime(String nome){
+
+    }
+}
---
+++ Java-Detail/src/br/com/abc/javacore/Jmodificadorfinal/test/CarroTest.java
@@ -0,0 +1,13 @@
+package br.com.abc.javacore.Jmodificadorfinal.test;
+
+import br.com.abc.javacore.Jmodificadorfinal.classes.Carro;
+
+public class CarroTest {
+    public static void main(String[] args) {
+        Carro c = new Carro();
+//        System.out.println(c.getComprador());
+//        c.getComprador().setNome("Kuririn");
+//        System.out.println(c.getComprador());
+
+    }
+}
