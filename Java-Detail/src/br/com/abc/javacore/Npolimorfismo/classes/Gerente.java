4830dda31e62f83d815e3295a79da28e4ed9d191
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2021-09-11 09:26:26 -0300

    Polimorfismo

---
+++ Java-Detail/src/br/com/abc/javacore/Npolimorfismo/classes/Gerente.java
@@ -0,0 +1,24 @@
+package br.com.abc.javacore.Npolimorfismo.classes;
+
+
+public class Gerente extends Funcionario {
+    private double pnl;
+
+    public Gerente(String nome, double salario, double pnl) {
+        super(nome, salario);
+        this.pnl = pnl;
+    }
+
+    @Override
+    public void calcularPagamento() {
+        this.salario = this.salario + pnl;
+    }
+
+    public double getPnl() {
+        return pnl;
+    }
+
+    public void setPnl(double pnl) {
+        this.pnl = pnl;
+    }
+}
