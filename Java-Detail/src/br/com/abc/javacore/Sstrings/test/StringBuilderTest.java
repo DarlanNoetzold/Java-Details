68c5d1ecce69e6fbcfba8673d9badd627f9facf3
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2021-09-20 08:12:43 -0300

    .

---
+++ Java-Detail/src/br/com/abc/javacore/Sstrings/test/StringBuilderTest.java
@@ -0,0 +1,9 @@
+package br.com.abc.javacore.Sstrings.test;
+
+public class StringBuilderTest {
+    public static void main(String[] args) {
+        String s = "Uma frase comum";
+        StringBuffer sb = new StringBuffer(16);
+        System.out.println(sb);
+    }
+}
