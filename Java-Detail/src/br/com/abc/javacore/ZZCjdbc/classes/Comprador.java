d5c73c7ad76126e5c2bc3c3ede6299e998880e18
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2021-10-22 07:29:14 -0300

    Create Comprador.java

---
+++ Java-Detail/src/br/com/abc/javacore/ZZCjdbc/classes/Comprador.java
@@ -0,0 +1,71 @@
+package br.com.abc.javacore.ZZCjdbc.classes;
+
+
+public class Comprador {
+    private Integer id;
+    private String cpf;
+    private String nome;
+
+    public Comprador() {
+    }
+
+    public Comprador(Integer id, String cpf, String nome) {
+        this.id = id;
+        this.cpf = cpf;
+        this.nome = nome;
+    }
+
+    public Comprador(String cpf, String nome) {
+        this.cpf = cpf;
+        this.nome = nome;
+    }
+
+    @Override
+    public String toString() {
+        return "Comprador{" +
+                "id=" + id +
+                ", cpf='" + cpf + '\'' +
+                ", nome='" + nome + '\'' +
+                '}';
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Comprador comprador = (Comprador) o;
+
+        return id != null ? id.equals(comprador.id) : comprador.id == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+}
