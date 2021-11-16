7b5da71cf5d559a93f86ed717437e8bf193a76cf
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2021-11-16 07:30:34 -0300

    Create AviaoSingletonTest.java

---
+++ Java-Detail/src/br/com/abc/javacore/ZZFpadroesdeprojeto/test/AviaoSingletonTest.java
@@ -0,0 +1,27 @@
+package br.com.abc.javacore.ZZFpadroesdeprojeto.test;
+
+import br.com.abc.javacore.ZZFpadroesdeprojeto.classes.AviaoSingleton;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
+public class AviaoSingletonTest {
+    public static void main(String[] args) throws IllegalAccessException, InvocationTargetException, InstantiationException {
+        agendarAssento("1A");
+        agendarAssento("1A");
+        AviaoSingleton aviaoSingleton = AviaoSingleton.getINSTANCE();
+        AviaoSingleton aviaoSingleton2 = null;
+        Constructor[] constructors = AviaoSingleton.class.getDeclaredConstructors();
+        for(Constructor constructor : constructors){
+            constructor.setAccessible(true);
+            aviaoSingleton2 = (AviaoSingleton) constructor.newInstance();
+            break;
+        }
+        System.out.println(aviaoSingleton.hashCode());
+        System.out.println(aviaoSingleton2.hashCode());
+    }
+    private static void agendarAssento(String assento) {
+        AviaoSingleton a = AviaoSingleton.getINSTANCE();
+        System.out.println(a.bookAssento(assento));
+    }
+}
