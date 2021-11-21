f1cdbda4674c27630186894824b94dcb51ec40af
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2021-11-21 08:01:38 -0300

    Update TestePessoa.java

--- Java-Detail/src/br/com/abc/javacore/ZZFpadroesdeprojeto/test/TestePessoa.java
+++ Java-Detail/src/br/com/abc/javacore/ZZFpadroesdeprojeto/test/TestePessoa.java
@@ -4,10 +4,10 @@ import br.com.abc.javacore.ZZFpadroesdeprojeto.classes.Pessoa;
 
 public class TestePessoa {
     public static void main(String[] args) {
-        Pessoa p = new Pessoa.PessoaBuilder("William")
-                .nomeDoMeio("Suane")
-                .ultimoNome("Queiroz")
-                .nomeDoPai("William")
+        Pessoa p = new Pessoa.PessoaBuilder("Darlan")
+                .nomeDoMeio("a")
+                .ultimoNome("Noetzold")
+                .nomeDoPai("Clovis")
                 .apelido("JirayaBolado")
                 .build();
         System.out.println(p);
