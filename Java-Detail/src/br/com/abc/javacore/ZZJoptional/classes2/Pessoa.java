8a5491e0b0c6198b3fbad0111f040f405e73d266
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2021-12-01 07:37:50 -0300

    Optional

---
+++ Java-Detail/src/br/com/abc/javacore/ZZJoptional/classes2/Pessoa.java
@@ -0,0 +1,22 @@
+package br.com.abc.javacore.ZZJoptional.classes2;
+
+public class Pessoa {
+    private Carro carro;
+    private String nome;
+
+    public Carro getCarro() {
+        return carro;
+    }
+
+    public void setCarro(Carro carro) {
+        this.carro = carro;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
---
+++ Java-Detail/src/br/com/abc/javacore/ZZJoptional/classes2/Seguradora.java
@@ -0,0 +1,14 @@
+package br.com.abc.javacore.ZZJoptional.classes2;
+
+
+public class Seguradora {
+    private String nome;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
