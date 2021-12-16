dc15b8092fb74d1d6643cd53985683834b09ecdf
Darlan Noetzold <darlan.noetzold@gmail.com>
2021-12-16 06:48:12 -0300

    Create FutureTest.java

---
+++ Java-Detail/src/br/com/abc/javacore/ZZMcompletablefuture/test/FutureTest.java
@@ -0,0 +1,33 @@
+package br.com.abc.javacore.ZZMcompletablefuture.test;
+
+import java.util.concurrent.*;
+
+public class FutureTest {
+    private static ExecutorService executorService = Executors.newFixedThreadPool(1);
+
+    public static void main(String[] args) {
+        Future<Double> future = executorService.submit(() -> {
+            TimeUnit.SECONDS.sleep(2);
+            return 2000D;
+        });
+        enrolando();
+        try {
+            while (!future.isDone()) {
+                Double resultado = future.get();
+                System.out.println(resultado);
+            }
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        } finally {
+            executorService.shutdown();
+        }
+    }
+
+    private static void enrolando() {
+        long soma = 0;
+        for (int i = 0; i < 1_000_000; i++) {
+            soma += i;
+        }
+        System.out.println(soma);
+    }
+}
