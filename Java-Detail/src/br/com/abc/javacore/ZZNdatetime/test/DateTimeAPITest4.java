bf0f2725db872dee899ae692b344fb59b86a20ce
Darlan Noetzold <darlan.noetzold@gmail.com>
2021-12-28 07:17:04 -0300

    Create DateTimeAPITest4.java

---
+++ Java-Detail/src/br/com/abc/javacore/ZZNdatetime/test/DateTimeAPITest4.java
@@ -0,0 +1,51 @@
+package br.com.abc.javacore.ZZNdatetime.test;
+
+import br.com.abc.javacore.ZZNdatetime.util.ObterProximoDiaUtil;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.temporal.TemporalAdjusters;
+
+public class DateTimeAPITest4 {
+    public static void main(String[] args) {
+        LocalDate date = LocalDate.now();
+        System.out.println(date);
+        System.out.println(date.getDayOfWeek());
+
+        date = date.with(TemporalAdjusters.nextOrSame(DayOfWeek.TUESDAY));
+        System.out.println(date);
+        System.out.println(date.getDayOfWeek());
+
+        date = date.with(TemporalAdjusters.next(DayOfWeek.TUESDAY));
+        System.out.println(date);
+        System.out.println(date.getDayOfWeek());
+
+        date = date.with(TemporalAdjusters.previous(DayOfWeek.TUESDAY));
+        System.out.println(date);
+        System.out.println(date.getDayOfWeek());
+
+        date = date.with(TemporalAdjusters.firstDayOfMonth());
+        System.out.println(date);
+        System.out.println(date.getDayOfWeek());
+
+        date = date.with(TemporalAdjusters.lastDayOfMonth());
+        System.out.println(date);
+        System.out.println(date.getDayOfWeek());
+
+        date = date.with(TemporalAdjusters.firstDayOfNextYear());
+        System.out.println(date);
+        System.out.println(date.getDayOfWeek());
+        date = date.with(new ObterProximoDiaUtil());
+        System.out.println(date);
+        System.out.println(date.getDayOfWeek());
+
+        date = date.plusDays(4);
+        System.out.println(date);
+        System.out.println(date.getDayOfWeek());
+
+        date = date.with(new ObterProximoDiaUtil());
+        System.out.println(date);
+        System.out.println(date.getDayOfWeek());
+
+    }
+}
